37d778e13e60f6a4c13f21a7cfe6fdbab2f24f83
MiloszDEV <miloszz.krakowiak@gmail.com>
2023-10-11 09:26:38 +0200

    dodanie pierwsze

---
+++ src/pl/gornik/Main.java
@@ -0,0 +1,10 @@
+package pl.gornik;
+
+public class Main {
+    public final static double LICZBA_PI = 3.14;
+    public static void main(String[] args) {
+        double radius = 4.25;
+        double areaCircle = LICZBA_PI * radius * radius;
+        System.out.println(areaCircle);
+    }
+}
---
+++ src/pl/gornik/vehicle/Car.java
@@ -0,0 +1,2 @@
+package pl.gornik.vehicle;public class Car {
+}
